e05c9f249ec7efe955f5a1d9263042bbd401fb96
Juan Oliveira <ruantos@proton.me>
2025-08-01 02:35:48 -0300

    fix: fixes script to call pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -2,18 +2,6 @@ import requests
 import pandas as pd
 import os
 
-URL = "http://dados.recife.pe.gov.br/api/3/action/datastore_search_sql?"
-SQL_QUERY = "sql=SELECT * FROM "
-
-DATA_DIR = "../data"
-RAW_DIR = os.path.join(DATA_DIR, "raw")
-
-id_list = {
-    "2023": "c269789d-da47-4dde-8ce7-42fba10fe8e2",
-    "2024": "4adf9430-35a5-4e88-8ecf-b45748b81c7d",
-    "2025": "48bd8822-df18-48d0-bbc1-2de87ca0d70b",
-    }
-
 
 class Pipeline:
 
@@ -98,8 +86,3 @@ class Pipeline:
 
     def pipeline_extraction(self) -> None:
         self.create_directories()
-
-
-if __name__ == "__main__":
-    ant = Pipeline(DATA_DIR, RAW_DIR, URL, SQL_QUERY, id_list)
-    ant.pipeline_extraction()
