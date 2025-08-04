8bcca45a440627fe8ea0709ec92aa67fde75572e
Juan Oliveira <ruantos@proton.me>
2025-08-04 20:51:08 -0300

    refactor: remove useless methods

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,30 +1,19 @@
 import requests
 import pandas as pd
-import os
 
 
 class Pipeline:
 
-    def __init__(self, data_dir: str,
-                 raw_dir: str,
-                 endpoint: str,
+    def __init__(self,
+                 url: str,
                  query: str,
                  ):
 
-        self.data_dir = data_dir
-        self.raw_dir = raw_dir
-        self.endpoint = endpoint
+        self.url = url
         self.query = query
 
-    def create_directories(self) -> None:
-        try:
-            os.makedirs(self.data_dir, exist_ok=True)
-            os.makedirs(self.raw_dir, exist_ok=True)
-        except OSError as e:
-            print(f"Error creating directory: {e}")
-
     def fetch_dataframe(self, id: str) -> pd.DataFrame:
-        url = f'{self.endpoint}{self.query}"{id}"'
+        url = f'{self.url}{self.query}"{id}"'
 
         try:
             response = requests.get(url, timeout=30)
@@ -72,12 +61,3 @@ class Pipeline:
         df = self.fix_columns(df)
         df = self.remove_garbage(df)
         return df
-
-    def save_dataframe(self, df: pd.DataFrame,
-                       year: str, path: str,) -> None:
-        if df is None:
-            print(f"The {year} dataframe wasnt saved due to an error")
-            return
-
-        df.to_csv(path, index=False)
-        print(f"CSV de {year} baixado com sucesso!")
