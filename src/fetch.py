cd96baa3c345a34c720829a49bdf542bf880e617
Juan Oliveira <ruantos@proton.me>
2025-08-19 14:19:05 -0300

    feat: keeps url and key in the function

--- src/fetch.py
+++ src/fetch.py
@@ -1,11 +1,15 @@
 import requests
 import pandas as pd
 
-def fetch_dataframe(url: str, id: str, query: str) -> pd.DataFrame | None:
-	url = f'{url}{query}"{id}"'
+
+def fetch_dataframe(id: str) -> pd.DataFrame | None:
+
+	url = 'http://dados.recife.pe.gov.br/api/3/action/datastore_search_sql?'
+	query = 'sql=SELECT * FROM '
+	full_url = f'{url}{query}"{id}"'
 
 	try:
-		response = requests.get(url, timeout=30)
+		response = requests.get(full_url, timeout=30)
 		return pd.DataFrame(response.json()["result"]["records"])
 
 	except requests.exceptions.RequestException as error:
