cca18d3dbc92487f0e4159bed10480299172a7ae
Juan Oliveira <ruantos@proton.me>
2025-08-22 15:54:20 -0300

    refactor: change id var name

--- src/extract.py
+++ src/extract.py
@@ -5,10 +5,10 @@ import pandas as pd
 URL = 'http://dados.recife.pe.gov.br/api/3/action/datastore_search_sql?'
 
 
-def fetch_dataframe(id: str) -> pd.DataFrame | None:
+def fetch_dataframe(identifier: str) -> pd.DataFrame | None:
 
 	query = 'sql=SELECT * FROM '
-	url_params = f'{URL}{query}"{id}"'
+	url_params = f'{URL}{query}"{identifier}"'
 
 	try:
 		response = requests.get(url_params,
@@ -16,7 +16,7 @@ def fetch_dataframe(id: str) -> pd.DataFrame | None:
 		records = response.json()["result"]["records"]
 
 		if not records:
-			print(f'No records found for dataset: {id}')
+			print(f'No records found for dataset: {identifier}')
 			return pd.DataFrame(records)
 		return pd.DataFrame(records)
 
