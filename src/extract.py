c27e61445080798b0782d06c7de3dd04caf7ccba
Juan Oliveira <ruantos@proton.me>
2025-08-22 15:50:15 -0300

    refactor: refactor fetch_dataframe

--- src/extract.py
+++ src/extract.py
@@ -2,15 +2,23 @@ import requests
 import pandas as pd
 
 
+URL = 'http://dados.recife.pe.gov.br/api/3/action/datastore_search_sql?'
+
+
 def fetch_dataframe(id: str) -> pd.DataFrame | None:
 
-	url = 'http://dados.recife.pe.gov.br/api/3/action/datastore_search_sql?'
 	query = 'sql=SELECT * FROM '
-	full_url = f'{url}{query}"{id}"'
+	url_params = f'{URL}{query}"{id}"'
 
 	try:
-		response = requests.get(full_url, timeout=60)
-		return pd.DataFrame(response.json()["result"]["records"])
+		response = requests.get(url_params,
+		                        timeout=60)
+		records = response.json()["result"]["records"]
+
+		if not records:
+			print(f'No records found for dataset: {id}')
+			return pd.DataFrame(records)
+		return pd.DataFrame(records)
 
 	except requests.exceptions.RequestException as error:
 		print(f"An error occurred while trying to fetch dataframe: {error}")
