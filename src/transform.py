a6cf2435b9c77c3776bb706d40b5008f8ec67806
Juan Oliveira <ruantos@proton.me>
2025-09-08 15:09:08 -0300

    feat: change pandas for duckdb on fix_cols method

--- src/transform.py
+++ src/transform.py
@@ -1,50 +1,30 @@
 import pandas as pd
+import duckdb
 
 
-class Transformer:
+def remove_garbage(df: duckdb.DuckDBPyRelation) -> pd.DataFrame:
+    df = df.drop_duplicates(
+        subset=["infracao", "hora", "ano", "mes"])
+
+    df = df.dropna(axis=0, subset=["infracao"])
+    return df
 
 
-    def __init__(self) -> None:
-        self.cols_to_drop = [
-        'dataimplantacao', 'descricaoinfracao', '_id',
-        'amparolegal', '_full_text',
-        'horainfracao', 'datainfracao'
-        ]
-
-    
-    def drop_columns(self, df: pd.DataFrame) -> pd.DataFrame:
-        return df.drop(columns=self.cols_to_drop)
-
-
-    def extract_date(self, df: pd.DataFrame) -> pd.DataFrame:
-        df['horainfracao'] = pd.to_datetime(df['horainfracao'],
-                                            format="%H:%M:%S")
-        df['datainfracao'] = pd.to_datetime(df['datainfracao'], yearfirst=True)
-
-        df["ano"] = df["datainfracao"].dt.year
-        df["mes"] = df["datainfracao"].dt.month
-        df["hora"] = df["horainfracao"].dt.hour
-
-        df["is_feriado"] = df["datainfracao"].dt.weekday > 4
-
-        return df
-
-    def fix_columns(self, df: pd.DataFrame) -> pd.DataFrame:
-        df = self.extract_date(df)
-        df = self.drop_columns(df)
-
-        return df
+def fix_columns(df: pd.DataFrame) -> duckdb.DuckDBPyRelation:
+    return duckdb.sql("""
+        SELECT 
+            AgenteEquipamento,
+            Infracao,
+            LocalCometimento,
+            YEAR( CAST(datainfracao AS DATE) ) AS ano,
+            MONTH( CAST(datainfracao AS DATE) ) AS mes,
+            HOUR( CAST(horainfracao AS TIME) ) AS hora,
+            ISODOW( CAST(datainfracao AS DATE)) > 5 AS is_feriado 
+        FROM df
+    """)
 
 
-    def remove_garbage(self, df: pd.DataFrame) -> pd.DataFrame:
-        df = df.drop_duplicates(
-            subset=["infracao", "hora", "ano", "mes"])
-
-        df = df.dropna(axis=0, subset=["infracao"])
-        return df
-
-
-    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
-        df = self.fix_columns(df)
-        df = self.remove_garbage(df)
-        return df
+def transform(df: pd.DataFrame) -> pd.DataFrame:
+    df = fix_columns(df)
+    df = remove_garbage(df)
+    return df
